4eaaa08e1f124396844e7944063a12a14066897d
Houman Hamed <h.hamed@anasys.ir>
2024-07-17 06:46:57 +0330

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
 import { DUMMY_USERS } from './dummy-users';
+import { TasksComponent } from "./tasks/tasks.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent],
+  imports: [HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
@@ -17,6 +18,6 @@ export class AppComponent {
     this.selectedUserId = id;
   }
   get sekectedUser(){
-    return this.users.find((user) => user.id === this.selectedUserId);
+    return this.users.find((user) => user.id === this.selectedUserId)!;
   }
 }
